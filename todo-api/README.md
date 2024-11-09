b0e8185826147b234a270095aec398065fadf794
cplan85 <carlos_planchart@live.com>
2024-11-09 20:30:57 +0100

    first commit

---
+++ todo-api/README.md
@@ -0,0 +1,11 @@
+# realtime-todo-task-management-app-nestjs-and-angular
+
+## How to run the Project & additioanl Docker commands
+1.Run all services: 'docker-compose up'
+
+## Setup Steps
+
+### NestJS Backend API
+
+### Angular Frontend
+
---
+++ todo-api/dist/user/user.service.d.ts
@@ -0,0 +1,12 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+export interface Test {
+    title: string;
+}
+export declare class UserService {
+    create(createUserDto: CreateUserDto): string;
+    findAll(): string;
+    findOne(id: number): Test;
+    update(id: number, updateUserDto: UpdateUserDto): string;
+    remove(id: number): string;
+}
